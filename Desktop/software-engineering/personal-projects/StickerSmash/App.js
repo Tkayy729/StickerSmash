2bbc0347d214ebc515d2c0d163669e46b18c80fd
Emmanuel Koduah Tweneboah <emmanueltweneboah71@gmail.com>
2023-08-29 16:45:52 +0000

    added an image picker from expo-image-picker

--- Desktop/software-engineering/personal-projects/StickerSmash/App.js
+++ Desktop/software-engineering/personal-projects/StickerSmash/App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View } from 'react-native';
 import ImageViewer from './src/components/ImageViewer';
+import Button from './src/components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer PlaceholderImage={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme={"primary"}   onPress={pickImageAsync} />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,5 +48,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   }
 })
